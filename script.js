ade4289900ad7191451107e27d0451f3211a2f77
Bruno Borici <brunoborici@gmail.com>
2025-01-30 18:12:56 +0100

    finalized everything but responsiveness

--- script.js
+++ script.js
@@ -95,3 +95,25 @@ function showToast(msg) {
         toast.remove();
     }, 2000);
 }
+
+// scroller Code
+const scrollers = document.querySelectorAll('.scroller');
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach(scroller => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector('.scroller-inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach((item) => {
+            const clone = item.cloneNode(true);
+            clone.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(clone);
+        });
+    });
+}
